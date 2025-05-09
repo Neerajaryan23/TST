920864b260d765a0d938e822077cf0f3d7b5df70
Neeraj <neerajkumararya147258@gmail.com>
2025-05-09 23:53:51 +0530

    TypingSpeedTest

--- first.js
+++ first.js
@@ -6,6 +6,8 @@ const WPM = document.querySelector(`.wpm span`);
 const CPM = document.querySelector(`.cpm span`);
 const btn = document.querySelector(`button`);
 
+
+
 let timer;
 let maxTime = 60;
 let timeLeft = maxTime;
@@ -14,7 +16,7 @@ let mistake = 0;
 let isTyping = false;
 
 function loadParagraph() {
-    const paragraph = ["Avoid daydreaming about the years to come.","You are the most important person in your whole life.","Always be true to who you are, and ignore what other people have to say about you.","Always be true to who you are, and ignore what other people have to say about you.","Only demonstrate your strength when it’s really required.","Whatever you can think you can do."];
+    const paragraph = ["Be at miss or each good play home they. It leave taste mr in it fancy. She son lose does fond bred gave lady get. Sir her company conduct expense bed any. Sister depend change off piqued one. Contented continued any happiness instantly objection yet her allowance. Use correct day new brought tedious. By come this been in. Kept easy or sons my it done.","Fog everywhere. Fog up the river, where it flows among green aits and meadows; fog down the river, where it rolls deified among the tiers of shipping and the waterside pollutions of a great (and dirty) city. Fog on the Essex marshes, fog on the Kentish heights. Fog creeping into the cabooses of collier-brigs; fog lying out on the yards and hovering in the rigging of great ships; fog drooping on the gunwales of barges and small boats.","It is Spring, moonless night in the small town, starless and bible-black, the cobblestreets silent and the hunched, courters'-and- rabbits' wood limping invisible down to the sloeblack, slow, black, crowblack, fishingboat-bobbing sea. The houses are blind as moles (though moles see fine to-night in the snouting, velvet dingles) or blind as Captain Cat there in the muffled middle by the pump and the town clock, the shops in mourning, the Welfare Hall in widows' weeds.", "Hush, the babies are sleeping, the farmers, the fishers, the tradesmen and pensioners, cobbler, schoolteacher, postman and publican, the undertaker and the fancy woman, drunkard, dressmaker, preacher, policeman, the webfoot cocklewomen and the tidy wives. Young girls lie bedded soft or glide in their dreams, with rings and trousseaux, bridesmaided by glow-worms down the aisles of the organplaying wood. The boys are dreaming wicked or of the bucking ranches of the night and the jollyrogered sea."];
 
     const randomIndex = Math.floor(Math.random()*paragraph.length);
     typingText.innerHTML = ``;
@@ -69,8 +71,6 @@ function initTime() {
     }
 }
 
-// const Time = document.querySelector(".time-element-class-or-id");
-
 function reset() {
     document.querySelector(".mistakes-display span").innerText = 0;
     loadParagraph();
@@ -85,8 +85,10 @@ function reset() {
     CPM.innerText = 0;
     mistakeDisplay.innerText = 0;
 }
-
 input.addEventListener(`input`,initTyping);
 btn.addEventListener(`click`,reset);
 loadParagraph();
+
+
+
 
